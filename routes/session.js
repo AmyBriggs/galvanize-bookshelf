2430bb65dcdc54c0f365787308152f3ab16cf47e
Amy Briggs <amylbd@gmail.com>
2016-09-23 11:08:13 -0600

    update knex version

--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,66 @@
 'use strict'
 
 const express = require('express')
-
-// eslint-disable-next-line new-cap
 const router = express.Router()
+const bcrypt = require('bcrypt-as-promised')
+const knex = require('../knex')
+// const humps = require('humps')
 
-// YOUR CODE HERE
+
+router.get('/', (req, res) => {
+    if (req.session.userID) {
+        res.send(true)
+    }
+    res.send(false)
+})
+
+router.post('/', function(req, res) {
+  knex('users').where('email', req.body.email).then(function(results) {
+    console.log(results)
+    if (!results) {
+      res.send('Username is already being used.')
+    } else {
+      var user = req.body
+      var hash = bcrypt.hash(req.body.password, 12)
+      knex('users')
+        .returning('*')
+        .insert({
+          email: user.email,
+          password: hash,
+        })
+        .then(function(results) {
+          console.log(results)
+          // delete results.password;
+          req.session.userInfo = results
+          res.send(user)
+        })
+    }
+  })
+})
+
+
+
+
+// router.post('/', (req, res) => {
+//     let user
+//     knex('users')
+//         .where('email', req.body.email)
+//         .first()
+//         .then(function(results) {
+//             if (!results) {
+//                 res.send('Please enter an email.')
+//             }
+//           user = humps.camelizeKeys(results)
+//         })
+//     var passwordMatch = bcrypt.compare(req.body.password, user.hashed_password)
+//     delete user.hashed_password
+//     if (passwordMatch === false) {
+//         res.send('Username or password incorrect.')
+//     } else {
+//         res.send(user)
+//     }
+// })
+
+
 
 module.exports = router
